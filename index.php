96f850f4000d3ea56d474c5874194f8382259a88
Raj Rana <be2020se715@gces.edu.np>
2023-07-09 23:24:29 +0545

    index.html deleted

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ if ($conn->connect_error) {
 }
 
 };
+
 ?>
 
 
@@ -51,7 +52,7 @@ if ($conn->connect_error) {
   <main>
     <div class="home_container section">
       <div class="sidebar">
-        <div >
+        <div>
             <a href="profile.html"><img src="svgs/user-svgrepo-com.png" class="profile_pic" alt=""></a>
         </div>
         <div>
@@ -71,11 +72,11 @@ if ($conn->connect_error) {
           </div>
           <div class="dropdown">
             <button onclick="dropDown()" class="dropbtn">Lists</button>
-            <div id="myDropdown" class="dropdown-content">
-              <a href="#">Personal</a>
-              <a href="#">College</a>
-              <a href="#">Work</a>
-            </div>
+              <div id="myDropdown" class="dropdown-content">
+                <a href="#">Personal</a>
+                <a href="#">College</a>
+                <a href="#">Work</a>
+              </div>
           </div>
         </div>
         <div class="my_lists day_select">
@@ -83,60 +84,83 @@ if ($conn->connect_error) {
         </div>
       </div>
       
-      <br><br>
       <div class="list_container section">
         <div class="welcome_text">
           <h1>Welcome, User</h1>
           <h1 class="period">.</h1>
-          
         </div>
         
-              <div class="tasks_container input-container">
-                <div>
+        <div>
+                
+          <button class="add_task_button" onclick="openForm()">Add Task</button>
 
-                </div>
-                <button class="add_task_button" onclick="openForm()">Add Task</button>
-
-                <div id="myModal" class="modal">
-                  <div class="task_form">
-                    <span onclick="closeForm()" style="float: right; cursor: pointer; color: #001233;">&times;</span>
-                    <h2>Task</h2>
-                    <form action="" method="post">
-                      <label for="task_name" class="form_label">Task Name:</label>
-                      <input type="text" id="task_name" name="task_name" required><br><br>
-                      <label for="description" class="form_label">Description</label>
-                      <input type="textarea" id="description" name="description"><br><br>
-                      <input type="submit" value="Add Task" class="add_task_submit">
-                    </form>
-                  </div>
-                </div>
+            <div id="myModal" class="modal">
+              <div class="task_form">
+                  <span onclick="closeForm()" style="float: right; cursor: pointer; color: #001233;">&times;</span>
+                  <h2>Task</h2>
+                  <form action="" method="post">
+                    <label for="task_name" class="form_label">Task Name:</label>
+                    <input type="text" id="task_name" name="task_name" required><br><br>
+                    <label for="description" class="form_label">Description</label>
+                    <input type="textarea" id="description" name="description"><br><br>
+                    <input type="submit" value="Add Task" class="add_task_submit">
+                  </form>
+              </div>
+            </div>
               
 
-                  <!-- <ul id="taskList"></ul> -->
+              <div class="task_container" style="overflow: auto; height: 35em;">
+                <?php
+                    if ($conn->connect_error) {
+                      die("Connection failed: " . $conn->connect_error);
+                    }
+                    
+                    // Retrieve data from the table
+                    $show_tasks = "SELECT task_name,description  FROM tasks";
+                    $result = $conn->query($show_tasks);
+                    
+                    if ($result->num_rows > 0) {
+                        // Generate HTML structure to display data
+                        echo '<div class="data-container">';
+                        
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<div class="task" onclick="openUpdateForm()">';
+                            echo 'Task: ' . $row["task_name"] . '<br>';
+                            echo 'Description: ' . $row["description"] . '<br>';
+                            echo '</div>';
+                        }
+                        
+                        echo '</div>';
+                    } else {
+                        echo "No data available.";
+                    }
+
+                ?>
+              <div id="updateForm" class="modal">
+                <div class="task_form">
+                  <span onclick="closeUpdateForm()" style="float: right; cursor: pointer; color: #001233;">&times;</span>
+                  <h2>Task</h2>
+                  <form action="" method="post">
+                    <label for="task_name" class="form_label">Task Name:</label>
+                    <input type="text" id="task_name" name="task_name" required><br><br>
+                    <label for="description" class="form_label">Description</label>
+                    <input type="textarea" id="description" name="description"><br><br>
+                    <input type="submit" value="Add Task" class="add_task_submit">
+                  </form>
+                </div>
+              </div>
+              </div>
                   <!-- <div class="task">
                     <a href=""></a>
                     <div class="round_checkbox"></div>
                     <span class="task_name">Sleep</span>
                   </div>
-                  <div class="task">
-                    <div class="round_checkbox"></div>
-                    <span class="task_name">Eat</span>
-                  </div>
-                  <div class="task">
-                    <div class="round_checkbox"></div>
-                    <span class="task_name">Gym</span>
-                  </div>
-                  <div class="task">
-                    <div class="round_checkbox"></div>
-                    <span class="task_name">Game</span>
-                  </div>
-                  <div class="task">
-                    <div class="round_checkbox"></div>
-                    <span class="task_name">Study</span>
-                  </div> -->
+                  -->
                 
         </div>
       </div>
+
+      <!-- --------------------------------------------------Right section--------------------------------------- -->
       <div class="right_container section">
         <div class="task_status_container">
           <div class="task_status">Tasks :0</div>
